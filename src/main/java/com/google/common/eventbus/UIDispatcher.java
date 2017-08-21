8784e38a0f03bf051db0fb03afb2bf2fd97cbe85
berndhopp <bernd@vaadin.com>
2017-08-22 00:05:17 +0200

    shorten code

--- src/main/java/com/google/common/eventbus/UIDispatcher.java
+++ src/main/java/com/google/common/eventbus/UIDispatcher.java
@@ -7,19 +7,15 @@ import java.util.Iterator;
 
 final class UIDispatcher extends Dispatcher {
     void dispatch(final Object event, Iterator<Subscriber> subscribers) {
-        while (subscribers.hasNext()) {
-            final Subscriber subscriber = subscribers.next();
-            if (subscriber.target instanceof Component) {
-                UI ui = ((Component) subscriber.target).getUI();
-                if (ui != null) {
-                    ui.access(() -> subscriber.dispatchEvent(event));
-                } else {
-                    subscriber.dispatchEvent(event);
-                }
+        for (Subscriber subscriber : (Iterable<Subscriber>) () -> subscribers) {
+
+            UI ui = subscriber.target instanceof Component ? ((Component) subscriber.target).getUI() : null;
+
+            if (ui != null) {
+                ui.access(() -> subscriber.dispatchEvent(event));
             } else {
                 subscriber.dispatchEvent(event);
             }
         }
-
     }
 }
